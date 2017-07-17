0f24cc3e3eb8b490f17f81dfcb5c24784f954ba1
Doowon Lee <duwonyi@gmail.com>
2017-07-17 10:42:52 -0400

    Add dependences for bootstrap

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 import CounterList from '../playground/CounterList'
 
 const HomePage = () => (
   <div>
     HomePage
+    <Button color="danger">Danger!</Button>
     <CounterList />
   </div>
 )
