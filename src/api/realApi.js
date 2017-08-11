01771eded9b336a83651d8331353674ffac30a91
Doowon Lee <duwonyi@gmail.com>
2017-08-11 11:24:36 -0400

    Edit API for auth

--- src/api/realApi.js
+++ src/api/realApi.js
@@ -2,43 +2,53 @@ const baseUrl = `http://${process.env.REACT_APP_API_SERVER}/api`
 
 const realApi = {
   loadSenetences: function() {
-    return get('/sentences')
+    return get('/sentences', true)
   },
 
   saveSentence: function(sentence) {
-    return post('/sentences', sentence)
+    return post('/sentences', sentence, true)
   },
 
   getSources: function(type) {
     if (type) {
-      return get(`/sources/types/${type}`)
+      return get(`/sources/types/${type}`, true)
     } else {
-      return get('/sources')
+      return get('/sources', true)
     }
   },
 
   saveSource: function(source) {
-    return post('/sources', source)
+    return post('/sources', source, true)
   },
 
   signin: function(creds) {
-    return post('/signin', creds)
+    return post('/signin', creds, false)
   },
 }
 
-function get(url) {
-  return fetch(baseUrl + url, { accept: 'application/json', })
+function get(url, isNeedToken) {
+  let config = {
+    accept: 'application/json',
+  }
+  if (isNeedToken) {
+    config['headers'] = getTokenHeader()
+  }
+  return fetch(baseUrl + url, config)
     .then(onSuccess, onError)
 }
 
-function post(url, data) {
+function post(url, data, isNeedToken) {
+  let headerConfig = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  if (isNeedToken) {
+    headerConfig['Authorization'] = `Bearer ${getToken()}`
+  }
   const request = new Request(baseUrl + url, {
     method: 'POST',
     body: JSON.stringify(data),
-    headers: new Headers({
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    })
+    headers: new Headers(headerConfig)
   })
   return fetch(request)
     .then(onSuccess, onError)
@@ -63,5 +73,11 @@ function convertStringToDate(json) {
     return json
   }
 }
+
+const getTokenHeader = () => ({
+    'Authorization': `Bearer ${getToken()}`
+})
+
+const getToken = () => localStorage.getItem('token') || null
 
 export default realApi
